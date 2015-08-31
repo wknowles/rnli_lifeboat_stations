56cc532242e54038136bdafc11dd6660bdf8b2e0
william knowles <onamatapier@gmail.com>
2015-08-31 22:28:39 +0100

    cludgy inport into sqlite

--- scraper.rb
+++ scraper.rb
@@ -1,44 +1,26 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
-
-# page to start scrape "http://rnli.org/aboutus/lifeboatsandstations/stations/Pages/Stations-a-z.aspx"
-# xpath "//*[starts-with(@id, "ctl00_PlaceHolderMain_PageRendererLoader_ctl00_StationAtoZ_RepeaterContainer")]" 
-
+require 'scraperwiki'
 require 'mechanize'
-require 'nokogiri'
 
-# Start mechanize
-mechanize = Mechanize.new
+ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
+
+# Start mechanize 
+agent = Mechanize.new
+
+# delay between requests 
+# agent.history_added = Proc.new { sleep 0.5 }
 
 # get first page with list of rnli stations
-first_page = mechanize.get('http://rnli.org/aboutus/lifeboatsandstations/stations/Pages/Stations-a-z.aspx')
-
+first_page = agent.get('http://rnli.org/aboutus/lifeboatsandstations/stations/Pages/Stations-a-z.aspx')
+  
 #list out links to station pages
 station_links = first_page.search "//*[contains(@id, 'StationNavHyperLink')]"
-station_links.each do |station_name|
-	# NOTE: actual link is in cat_link["href"]
-	puts station_name.text
+station_links.each do |station_link|
+
+# Write out to the sqlite database using scraperwiki library
+# This is a little wierd - it saves everything in station_link not just href
+	ScraperWiki.save_sqlite(['href'], station_link)
+
+#print out result
+#puts station_link.text
+
 end
