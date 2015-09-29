49fd6c4f9c909b6bef789dfe87ebb2c0a572e210
william knowles <onamatapier@gmail.com>
2015-09-29 16:33:17 +0100

    ready to go

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
 require 'mechanize'
-require 'json'
-#require 'scraperwiki'
+#require 'json'
+require 'scraperwiki'
 
-#ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
+ScraperWiki.config = { db: 'data.sqlite', default_table_name: 'data' }
 
 mechanize = Mechanize.new
+mechanize.user_agent_alias = 'Mac Safari'
 puts "Fetching Main Page ...\n"
 page = mechanize.get('http://rnli.org/aboutus/lifeboatsandstations/stations/Pages/Stations-a-z.aspx')
 
@@ -14,13 +15,13 @@ puts "Finding Station Links\n"
 station_links = page.links_with(href: %r{^\/findmynearest\/station\/Pages\/.*})
 puts "Number of Station Links => #{station_links.length}"
 
-station_links = station_links[0...2]
-puts "Selecting #{station_links.count} stations\n"
+#station_links = station_links[0...4]
+#puts "Selecting #{station_links.count} stations\n"
 
 stations = station_links.map.with_index do |link, index|
-  puts "Following Link to #{link} ... #{index}"
+  puts "#{index+1} - #{link}"
   station = link.click
-  station_name = station.search('.descriptiveTitle h1')[0].text
+  station_name = station.search('.descriptiveTitle h1').text
   station_address =  station.search('.ms-rteTable-0 .ms-rteElement-P')[1].text.gsub(/([a-z])([A-Z])/, '\1, \2')
   station_telephone = station.search('.ms-rteTable-0 .ms-rteElement-P')[3].text.gsub(/\s+/,'')
   {
@@ -30,10 +31,10 @@ stations = station_links.map.with_index do |link, index|
   }
 end
 
-# puts "Saving to data.sqlite\n"
-#
-# stations.each do |station|
-#   ScraperWiki.save_sqlite([:station_name, :station_address], station)
-# end
+puts "Saving to data.sqlite\n"
 
-puts JSON.pretty_generate(stations)
+stations.each do |station|
+  ScraperWiki.save_sqlite([:station_name, :station_address], station)
+end
+
+#puts JSON.pretty_generate(stations)
